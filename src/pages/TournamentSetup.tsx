9a5dec5f01652571c20be0f32aa3d64bb1b22b8c
deliggi-zx <deliggi@abc.gob.ar>
2026-06-04 10:31:36 -0300

    feat: nueva identidad Go Polo con logo actualizado

--- src/pages/TournamentSetup.tsx
+++ src/pages/TournamentSetup.tsx
@@ -227,7 +227,7 @@ export default function TournamentSetup({ onCreated }: Props) {
       <div style={{ display: 'flex', justifyContent: 'center', marginBottom: 16 }}>
         <img src="/logo.jpg" alt="Tribu de Polo" style={{ width: 80, height: 80, borderRadius: 12, objectFit: 'cover' }} />
       </div>
-      <h1 style={styles.title}>TRIBU POLO</h1>
+      <h1 style={styles.title}>GO POLO</h1>
       <p style={styles.sub}>Configuracion del torneo</p>
 
       {/* Stepper */}
